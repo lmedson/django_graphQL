177ef06ca16d85a35bcf402a315aafda1490e20b
lmedson <medson-mbp@hotmail.com>
2018-04-28 11:40:28 -0300

    #03:Trabalhando com mutations

--- hackernews/schema.py
+++ hackernews/schema.py
@@ -6,5 +6,11 @@ class Query(links.schema.Query, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(links.schema.Mutation, graphene.ObjectType):
+    pass
+
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+
 

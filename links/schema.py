a75833aa4e34224b67a3acba09b36794dec8aaef
lmedson <medson-mbp@hotmail.com>
2018-04-28 11:47:38 -0300

    04:Comments

--- links/schema.py
+++ links/schema.py
@@ -15,18 +15,24 @@ class Query(graphene.ObjectType):
     def resolve_links(self, info, **kwargs):
         return Link.objects.all()
 
-
+''' define uma classe mutação, em seguida, definida a saída da mutação, 
+    os dados que o servidor poderá retornar ao user. A saída é definida 
+    campo a campo para fins de aprendizado. Na próxima mutação, serão definidos como apenas um.'''
 class CreateLink(graphene.Mutation):
     id = graphene.Int()
     url = graphene.String()
     description = graphene.String()
 
     #2
+    '''Define os dados que podem ser enviados para o servidor, nesse caso, o URL e a descrição dos links.'''
     class Arguments:
         url = graphene.String()
         description = graphene.String()
 
     #3
+    '''O método de mutação: cria um link no banco de dados usando os dados enviados pelo usuário, através dos parâmetros
+        url e description. Depois, o servidor retorna a classe CreateLink com os dados que acabaram de ser criados. Veja como 
+        isso corresponde aos parâmetros definidos em # 1.'''
     def mutate(self, info, url, description):
         link = Link(url=url, description=description)
         link.save()

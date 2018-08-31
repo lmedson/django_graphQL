80882d1f3103c30e2fe34b1a78e978c0b43a22d6
Medson Mendes <lmedson@users.noreply.github.com>
2018-08-31 20:05:00 -0300

    Update settings.py

--- hackernews/settings.py
+++ hackernews/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+#STATICFILES_DIRS = (
+#    os.path.join(PROJECT_ROOT, 'static'),
+#)
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'

48f6721d0d1e3975e284520a422518ea1d5f11e7
lmedson <medson-mbp@hotmail.com>
2018-08-31 19:17:25 -0300

    add files to heroku

--- hackernews/settings.py
+++ hackernews/settings.py
@@ -133,3 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
